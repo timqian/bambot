5cf19b8570ce006caa70cbe8e322efd7b3484938
timqian <timqian@t9t.io>
2025-04-30 17:15:27 +0800

    add analytics

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,7 @@ export default function RootLayout({
       <body className={`${inter.className} bg-black text-white`}>
         <Header />
         {children}
-        {/* Remove the GitHub buttons script here as it's handled in Header.tsx */}
+        <Analytics />
       </body>
     </html>
   );
