4a6fe1abc049a8560002e321f27aaaec96ce7a9d
timqian <timqian@t9t.io>
2025-04-30 17:50:36 +0800

    Refactor generateMetadata to ensure params are resolved asynchronously

--- website/app/play/[slug]/layout.tsx
+++ website/app/play/[slug]/layout.tsx
@@ -1,13 +1,13 @@
 export async function generateMetadata({
   params,
 }: {
-  params: { slug: string }
+  params: { slug: string };
 }) {
-  const slug = params.slug as string;
+  const { slug } = await Promise.resolve(params); // 确保 params 是异步的
   return {
     title: `Play with ${slug}`,
     description: `${slug} simulation and control`,
-  }
+  };
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
